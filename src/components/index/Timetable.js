9aa1e4f8c60da1b2bd05d970d4a786c34cc8822e
Keerati <keerati.jear@gmail.com>
2018-07-09 23:39:14 +0700

    Update sample data and Link to timetable

--- src/components/index/Timetable.js
+++ src/components/index/Timetable.js
@@ -1,7 +1,27 @@
 import React from "react"
+import { Container, Row } from "reactstrap"
+import { Element } from "react-scroll"
+
+import { Col } from "../bases/Layout"
+
+// eslint-disable-next-line
+const sessions = [{"time":"13:00 - 13:30","session":"ลงทะเบียน"},{"time":"13:30 - 14:00","session":"พิธีเปิดโครงการ"},{"time":"14:00 - 14:35","session":"Session I"},{"time":"14:40 - 15:15","session":"Session II"},{"time":"15:15 - 15:30","session":"พักรับประทานอาหารว่าง และแลกเปลี่ยนกันตามอัธยาศัย"},{"time":"15:30 - 16:05","session":"Session III"},{"time":"16:10 - 16:30","session":"สรุปงานและแลกเปลี่ยนกันตามอัธยาศัย"}]
 
 const Timetable = () => (
-  <div>TimeTable</div>
+  <Container>
+    <Row>
+      <Col>
+        <Element name='timetable'>
+          <h1>Timetable</h1>
+          <article>
+            {
+              sessions.map(({session, time}) => <div key={`${time}${session}`}>{`${time} ${session}`}</div>)
+            }
+          </article>
+        </Element>
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default Timetable
