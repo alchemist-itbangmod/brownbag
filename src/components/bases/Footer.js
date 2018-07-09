974bce317414ec096e2c26ea95feccbcfcb0f2c4
Keerati <keerati.jear@gmail.com>
2018-07-09 23:36:50 +0700

    Update Footer directory

---
+++ src/components/bases/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Row } from "reactstrap"
+
+import { Col } from "./Layout"
+
+const Footer = () => (
+  <Row>
+    <Col>
+      Footer
+    </Col>
+  </Row>
+)
+export default Footer
