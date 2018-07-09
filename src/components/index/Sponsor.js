a12b9b4398ecace9f53a4a5a7a27f4d346cf8af6
Keerati <keerati.jear@gmail.com>
2018-07-09 23:40:01 +0700

    Update element link to sponsor

--- src/components/index/Sponsor.js
+++ src/components/index/Sponsor.js
@@ -1,9 +1,19 @@
 import React from "react"
+import { Container, Row } from "reactstrap"
+import { Element } from "react-scroll"
+
+import { Col } from "../bases/Layout"
 
 const Sponsor = () => (
-  <div>
-    Sponsor
-  </div>
+  <Container>
+    <Row>
+      <Col>
+        <Element name='sponsor'>
+          <h1>Sponsor</h1>
+        </Element>
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default Sponsor
