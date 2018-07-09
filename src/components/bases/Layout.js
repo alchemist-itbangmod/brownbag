d04d4b7a9505315a334f9746b057efa86934a789
Keerati <keerati.jear@gmail.com>
2018-07-09 23:37:44 +0700

    Update layout with move navbar and footer outside

--- src/components/bases/Layout.js
+++ src/components/bases/Layout.js
@@ -1,12 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 
-import React from "react"
-import { Container } from "reactstrap"
+import React, { Component, Fragment } from "react"
+import { Container, Col as DefaultCol } from "reactstrap"
 
-const Layout = ({ children }) => (
-  <Container fluid>
-    { children }
-  </Container>
-)
+import injectGlobal from "./InjectGlobal"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
-export default Layout
+export default class Layout extends Component {
+  componentWillMount = async () => {
+    injectGlobal()
+  }
+
+  render () {
+    const { children } = this.props
+    return (
+      <Fragment>
+        <Navbar />
+        <Container fluid>
+          { children }
+        </Container>
+        <Footer />
+      </Fragment>
+    )
+  }
+}
+
+const Col = props => <DefaultCol {...props} />
+
+Col.defaultProps = {
+  xs: "12"
+}
+
+export {
+  Col
+}
