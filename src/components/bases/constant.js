ce6c0ad8042f760abbda990d11b7adaacc1c8769
Keerati <keerati.jear@gmail.com>
2018-07-09 23:37:16 +0700

    Add constant color, fontsize and margin

---
+++ src/components/bases/constant.js
@@ -0,0 +1,28 @@
+const FONT_SIZE = {
+  small: "0.7em",
+  normal: "1.4em",
+  large: "2em",
+  header: "3em",
+  subheader: "2.5em"
+}
+
+const COLOR = {
+  primary: "rgb(74,44,44)",
+  primaryFade: "rgba(74, 44, 44, 0.65)",
+  fontPrimary: "#fff"
+}
+
+const MARGIN = {
+  small: "1em",
+  normal: "3em",
+  large: "5em"
+}
+
+const PADDING = MARGIN
+
+export {
+  FONT_SIZE,
+  COLOR,
+  MARGIN,
+  PADDING
+}
