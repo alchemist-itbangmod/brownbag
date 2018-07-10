c7965d45f5c5969d6e1cbd3ab7a248a2e8d2d02a
Keerati <keerati.jear@gmail.com>
2018-07-10 12:08:07 +0700

    Update style and refactor code

--- src/components/index/Sponsor.js
+++ src/components/index/Sponsor.js
@@ -1,19 +1,54 @@
-import React from "react"
-import { Container, Row } from "reactstrap"
+import React, { Fragment } from "react"
+import styled from "styled-components"
+import { Container } from "reactstrap"
 import { Element } from "react-scroll"
 
+import { COLOR } from "../bases/constant"
 import { Col } from "../bases/Layout"
 
+const sponsors = [
+  "/static/images/kmutt.svg",
+  "/static/images/sit.svg"
+]
+
+const Image = styled.img`
+  max-width: 250px;
+  max-height: 150px;
+`
+
+const SponsorContainer = styled(Container)`
+  background-color: ${COLOR.secondary};
+`
+
+const SponsorCol = styled(Col)`
+`
+SponsorCol.defaultProps = {
+  className: "py-5 d-flex flex-column justify-content-center align-items-center"
+}
+
+const SponsorList = ({ sponsors }) => (
+  <Fragment>
+    {
+      sponsors.map(sponsor => <Image key={sponsor} src={sponsor} />)
+    }
+  </Fragment>
+)
+
+SponsorList.defaultProps = {
+  sponsors
+}
+
 const Sponsor = () => (
-  <Container>
-    <Row>
-      <Col>
-        <Element name='sponsor'>
+  <SponsorContainer fluid>
+    <Container>
+      <Element className='row' name='sponsor'>
+        <SponsorCol>
           <h1>Sponsor</h1>
-        </Element>
-      </Col>
-    </Row>
-  </Container>
+          <SponsorList />
+        </SponsorCol>
+      </Element>
+    </Container>
+  </SponsorContainer>
 )
 
 export default Sponsor
--- src/components/index/Timetable.js
+++ src/components/index/Timetable.js
@@ -1,27 +1,45 @@
 import React from "react"
+import styled from "styled-components"
 import { Container, Row } from "reactstrap"
 import { Element } from "react-scroll"
 
+import { COLOR } from "../bases/constant"
 import { Col } from "../bases/Layout"
 
-// eslint-disable-next-line
-const sessions = [{"time":"13:00 - 13:30","session":"ลงทะเบียน"},{"time":"13:30 - 14:00","session":"พิธีเปิดโครงการ"},{"time":"14:00 - 14:35","session":"Session I"},{"time":"14:40 - 15:15","session":"Session II"},{"time":"15:15 - 15:30","session":"พักรับประทานอาหารว่าง และแลกเปลี่ยนกันตามอัธยาศัย"},{"time":"15:30 - 16:05","session":"Session III"},{"time":"16:10 - 16:30","session":"สรุปงานและแลกเปลี่ยนกันตามอัธยาศัย"}]
+const sessions = [
+  {"time": "13:00 - 13:30", "session": "ลงทะเบียน"},
+  {"time": "13:30 - 14:00", "session": "พิธีเปิดโครงการ"},
+  {"time": "14:00 - 14:35", "session": "Session I"},
+  {"time": "14:40 - 15:15", "session": "Session II"},
+  {"time": "15:15 - 15:30", "session": "พักรับประทานอาหารว่าง และแลกเปลี่ยนกันตามอัธยาศัย"},
+  {"time": "15:30 - 16:05", "session": "Session III"},
+  {"time": "16:10 - 16:30", "session": "สรุปงานและแลกเปลี่ยนกันตามอัธยาศัย"}
+]
+
+const TimetbleContainer = styled(Container)`
+  background-color: ${COLOR.normal};
+`
+TimetbleContainer.defaultProps = {
+  className: "py-5"
+}
 
 const Timetable = () => (
-  <Container>
-    <Row>
-      <Col>
-        <Element name='timetable'>
+  <TimetbleContainer fluid>
+    <Container >
+      <Element className='row' name='timetable'>
+        <Col className='text-center'>
           <h1>Timetable</h1>
-          <article>
+        </Col>
+        <Col>
+          <span>
             {
               sessions.map(({session, time}) => <div key={`${time}${session}`}>{`${time} ${session}`}</div>)
             }
-          </article>
-        </Element>
-      </Col>
-    </Row>
-  </Container>
+          </span>
+        </Col>
+      </Element>
+    </Container>
+  </TimetbleContainer>
 )
 
 export default Timetable
