b584004ee27b97ef5fe9e9764cdbd4bee2429e4b
Keerati <keerati.jear@gmail.com>
2018-07-10 12:06:58 +0700

    Update footer style

--- src/components/bases/Footer.js
+++ src/components/bases/Footer.js
@@ -5,6 +5,11 @@ import { Container, Row } from "reactstrap"
 import { COLOR } from "./constant"
 import { Col } from "./Layout"
 
+const Logo = styled.img`
+  max-height: 60px;
+  cursor: pointer;
+`
+
 const FooterContainer = styled(Container)`
   background-color: ${COLOR.primary};
   color: ${COLOR.fontPrimary};
@@ -17,9 +22,24 @@ FooterContainer.defaultProps = {
 const Footer = () => (
   <FooterContainer fluid>
     <Container>
-      <Row>
-        <Col>
-          Footer
+      <Row className='d-flex justify-content-between'>
+        <Col lg='2' className='text-center'>
+          <h4>Brown Bag</h4>
+          <h6>The Fourth</h6>
+        </Col>
+        <Col lg='2' className='text-center'>
+          <h6>Event Organizer by</h6>
+          <a href='https://www.facebook.com/alchemistitbangmod/' target='_blank'>
+            <Logo src='/static/images/alchemist.png' />
+          </a>
+        </Col>
+        <Col lg='2' className='text-center'>
+          <h5>CONNECT WITH US</h5>
+          <a href='https://www.facebook.com/alchemistitbangmod/' target='_blank'>
+            <Logo src='/static/images/fbicon.png' />
+          </a>
+          <br />
+          <a href='mailto:alchemist@sit.kmutt.ac.th'>alchemist@sit.kmutt.ac.th</a>
         </Col>
       </Row>
     </Container>
