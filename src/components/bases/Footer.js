aa8f2198ef4a963788bc8da78f217e079702a36c
Keerati <keerati.jear@gmail.com>
2018-07-10 11:02:10 +0700

    Update footer style

--- src/components/bases/Footer.js
+++ src/components/bases/Footer.js
@@ -1,13 +1,28 @@
 import React from "react"
-import { Row } from "reactstrap"
+import styled from "styled-components"
+import { Container, Row } from "reactstrap"
 
+import { COLOR } from "./constant"
 import { Col } from "./Layout"
 
+const FooterContainer = styled(Container)`
+  background-color: ${COLOR.primary};
+  color: ${COLOR.fontPrimary};
+`
+
+FooterContainer.defaultProps = {
+  className: "py-4"
+}
+
 const Footer = () => (
-  <Row>
-    <Col>
-      Footer
-    </Col>
-  </Row>
+  <FooterContainer fluid>
+    <Container>
+      <Row>
+        <Col>
+          Footer
+        </Col>
+      </Row>
+    </Container>
+  </FooterContainer>
 )
 export default Footer
