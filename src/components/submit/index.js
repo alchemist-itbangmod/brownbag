e93cca382bc24219c1a8453b90cbbeffc5d03e17
Keerati <keerati.jear@gmail.com>
2018-07-11 22:41:20 +0700

    Update submit index style

--- src/components/submit/index.js
+++ src/components/submit/index.js
@@ -4,19 +4,20 @@ import styled from "styled-components"
 import Layout from "../bases/Layout"
 import { FONT_SIZE, FLEX, COLOR } from "../bases/constant"
 
-const heightSize = "75vh"
+const heightSize = "85vh"
 
 const SubmitOverlay = styled.div`
   background: url('static/images/IMG_3967.JPG');
-  height: ${heightSize};
+  min-height: ${heightSize};
 `
 
 const SubmitSection = styled.div`
   font-size: ${FONT_SIZE.hero};
   background: ${COLOR.primaryFade};
   color: ${COLOR.fontPrimary};
-  height: ${heightSize};
+  min-height: ${heightSize};
   margin-top: -${heightSize};
+
 `
 
 export default class Submit extends Component {
