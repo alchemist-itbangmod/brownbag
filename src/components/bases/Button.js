7514c2ba2dc1580b3616a6f5d80e3d97c1f90326
Keerati <keerati.jear@gmail.com>
2018-07-11 22:40:38 +0700

    Add facebook Button

--- src/components/bases/Button.js
+++ src/components/bases/Button.js
@@ -1,7 +1,7 @@
 import { Button } from "reactstrap"
 import styled from "styled-components"
 
-import { FONT_SIZE, COLOR, MARGIN } from "./constant"
+import { FONT_SIZE, COLOR, MARGIN, PADDING } from "./constant"
 
 const ButtonPrimary = styled(Button)`
   border-radius: 0px;
@@ -29,7 +29,26 @@ const ButtonPrimaryInverted = styled(ButtonPrimary)`
   border: 3px solid ${COLOR.highlight} !important;
 `
 
+const FacebookButton = styled.button`
+  background-color: ${COLOR.facebook};
+  color: ${COLOR.fontPrimary};
+  font-size: ${FONT_SIZE.mini};
+  cursor: pointer;
+  border: none;
+  padding: 0.3em 1em;
+  border-radius: 8px;
+  transition: .2s;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+  &:active {
+    transform: scale(1.05);
+  }
+`
+
 export default ButtonPrimary
 export {
-  ButtonPrimaryInverted
+  ButtonPrimaryInverted,
+  FacebookButton
 }
