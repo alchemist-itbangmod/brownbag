dcfcb5a79bc45a44275f62a8b5a06ad396ba2e48
Keerati <keerati.jear@gmail.com>
2018-07-11 11:52:25 +0700

    Update timetable list

--- src/components/index/Timetable.js
+++ src/components/index/Timetable.js
@@ -7,13 +7,15 @@ import { COLOR } from "../bases/constant"
 import { Fluid, Col } from "../bases/Layout"
 
 const sessions = [
-  {"time": "13:00 - 13:30", "session": "ลงทะเบียน"},
-  {"time": "13:30 - 14:00", "session": "พิธีเปิดโครงการ"},
+  {"time": "13:00 - 13:30", "session": "Register"},
+  {"time": "13:30 - 14:00", "session": "Opening Ceremony"},
   {"time": "14:00 - 14:35", "session": "Session I"},
+  {"time": "14:35 - 14:40", "session": "Move to Session II"},
   {"time": "14:40 - 15:15", "session": "Session II"},
-  {"time": "15:15 - 15:30", "session": "พักรับประทานอาหารว่าง และแลกเปลี่ยนกันตามอัธยาศัย"},
+  {"time": "15:15 - 15:30", "session": "Take a break and Talk together"},
   {"time": "15:30 - 16:05", "session": "Session III"},
-  {"time": "16:10 - 16:30", "session": "สรุปงานและแลกเปลี่ยนกันตามอัธยาศัย"}
+  {"time": "16:05 - 16:10", "session": "Move to Closing Ceremony"},
+  {"time": "16:10 - 17:00", "session": "Closing Ceremony"}
 ]
 
 const Timetable = () => (
