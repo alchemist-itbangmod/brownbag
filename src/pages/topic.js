30eb98da9938b56b77b6ffaf2d597a138464c9f2
Keerati <keerati.jear@gmail.com>
2018-08-04 10:15:34 +0700

    Add alias topic endpoint

---
+++ src/pages/topic.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import Topic from "../components/topic"
+
+export default class Topics extends Component {
+  render () {
+    return (
+      <Topic />
+    )
+  }
+}
